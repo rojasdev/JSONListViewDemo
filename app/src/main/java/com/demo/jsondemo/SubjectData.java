062ea35797b9aab3f6df4a7dd043d2e50f0c1b0f
rojasdev <r.rojas@usls.edu.ph>
2022-12-12 12:02:50 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/jsondemo/SubjectData.java
@@ -0,0 +1,13 @@
+package com.demo.jsondemo;
+
+class SubjectData {
+    String SubjectName;
+    String Number;
+    String Image;
+
+    public SubjectData(String subjectName, String number, String image) {
+        this.SubjectName = subjectName;
+        this.Number = number;
+        this.Image = image;
+    }
+}
